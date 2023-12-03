708f473087533a746c34dc468ecce590a0bb2ad7
Mohamed Rezk <mhrezk5@hotmail.com>
2023-12-03 14:49:41 -0500

    Added project files

---
+++ veltahleon/src/main/java/com/testament/veltahleon/VeltahleonApplication.java
@@ -0,0 +1,13 @@
+package com.testament.veltahleon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VeltahleonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VeltahleonApplication.class, args);
+	}
+
+}
---
+++ veltahleon/src/test/java/com/testament/veltahleon/VeltahleonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testament.veltahleon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VeltahleonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
